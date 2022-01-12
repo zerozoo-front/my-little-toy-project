dd78836f647dd67e2efb2c32d21f537104ff0fae
윤영주 <user@yun-yeongjuui-MacBookAir.local>
2022-01-12 23:19:37 +0900

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
-import type { Express, Request, Response } from 'express';
+import type { Express, Request, Response, NextFunction } from 'express';
 
 const PORT: number = 3000;
 
 const app: Express = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('middle ware 1');
+  setTimeout((): void => {
+    next();
+    console.log('wait 1 sec');
+  }, 1000);
+});
 app.use('/', (req: Request, res: Response): void => {
+  console.log('middle ware 2');
   res.status(200).send('Hello! express');
 });
 
